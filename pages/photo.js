cf825d376cab0dc2eb1259bd66c99db1e02c1830
Julian Bustos <julian.m.bustos@gmail.com>
2021-07-18 17:25:38 -0400

    added hoveron and modal for photo gallery

--- pages/photo.js
+++ pages/photo.js
@@ -1,10 +1,14 @@
 import styled from "@emotion/styled";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import Modal from "../components/Modal";
+import { Grid } from "react-awesome-spinners";
+
 
 
 const PhotoWrapper = styled.main`
 padding: 1rem;
+position: relative;
 * {
   /* border: 1px solid; */
 }
@@ -16,6 +20,14 @@ section {
   /* background-color: red; */
   justify-content: center;
   align-content: center;
+  .spinner {
+    position: relative;
+    margin: 0 auto;
+    z-index: 0;
+    background: red;
+    width: fit-content;
+    height: fit-content;
+  }
 }
 `;
 
@@ -38,14 +50,18 @@ const PhotoNav = styled.div`
 
 
 const ImageContainer = styled.div`
-  width: ${({ width }) => width };
-  width: ${({ height }) => height };
+  width: ${({ width }) => width};
+  width: ${({ height }) => height};
   display: flex;
   justify-content: center;
   margin: 0 auto;
+  transition: all .2s ease-in-out;
   /* background: red; */
   img {
     /* margin: 0 auto; */
+  }
+  &:hover {
+    transform: scale(1.05);
   }
 `;
 
@@ -65,6 +81,10 @@ const Photo = () => {
   const [loading, setIsLoading] = useState(true);
   const [weddingImages, setWeddingImages] = useState([]);
   const [portraitImages, setPortraitImages] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalInfo, setModalInfo] = useState({
+
+  });
 
 
 
@@ -89,8 +109,32 @@ const Photo = () => {
     return () => {
     }
   }, []);
+  useEffect(() => {
+    const handleEsc = (event) => {
+      if (event.keyCode === 27) {
+        setIsModalOpen(false);
+      }
+    };
+    window.addEventListener('keydown', handleEsc);
+
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  }, []);
+
 
   const modeClickHandler = (mode) => setMode(mode);
+  const modalClickHandler = (modalInfo, imageDimensions) => {
+    if (isModalOpen) {
+      setIsModalOpen(!isModalOpen);
+      setModalInfo({})
+    }
+    setIsModalOpen(!isModalOpen);
+    setModalInfo({
+      imageId: modalInfo,
+      imageDimensions: imageDimensions
+    });
+  }
 
   return (
     <PhotoWrapper>
@@ -104,40 +148,44 @@ const Photo = () => {
       </PhotoNav>
       <section>
         {loading ? (
-          <h1>Loading!!!</h1>
+          <div className="spinner">
+            <Grid color="black" />
+          </div>
         ) : (
           <>
             {mode !== pageOptions[2] && weddingImages.map((image, index) => {
-              const imageOrientation = image.width/image.height > 1 ? "horizontal" : "vertical";
+              const imageOrientation = image.width / image.height > 1 ? "horizontal" : "vertical";
               const imageDimensions = {
-                width:  imageOrientation === "horizontal" ? "600px" : "267px",
+                width: imageOrientation === "horizontal" ? "600px" : "267px",
                 height: imageOrientation === "horizontal" ? "400px" : "400px"
               }
               console.log(image.public_id)
               return (
-              <ImageContainer key={index}  width={imageDimensions.width} height={imageDimensions.height}>
-                <Image  src={`https://res.cloudinary.com/julianb/image/upload/h_400/${image.public_id}.jpg`} alt={""} width={imageDimensions.width} height={imageDimensions.height} />
-                {console.log({imageOrientation})}
-              </ImageContainer>
-            )})}
+                <ImageContainer key={index} onClick={() => modalClickHandler(image.public_id, imageDimensions)} width={imageDimensions.width} height={imageDimensions.height}>
+                  <Image src={`https://res.cloudinary.com/julianb/image/upload/h_400/${image.public_id}.jpg`} alt={""} width={imageDimensions.width} height={imageDimensions.height} />
+                </ImageContainer>
+              )
+            })}
             {mode !== pageOptions[1] && portraitImages.map((image, index) => {
-              const imageOrientation = image.width/image.height > 1 ? "horizontal" : "vertical";
+              const imageOrientation = image.width / image.height > 1 ? "horizontal" : "vertical";
               const imageDimensions = {
-                width:  imageOrientation === "horizontal" ? "600px" : "267px",
+                width: imageOrientation === "horizontal" ? "600px" : "267px",
                 height: imageOrientation === "horizontal" ? "400px" : "400px"
               }
               console.log(image.public_id)
               return (
-              <ImageContainer key={index}  width={imageDimensions.width} height={imageDimensions.height}>
-                <Image  src={`https://res.cloudinary.com/julianb/image/upload/h_400/${image.public_id}.jpg`} alt={""} width={imageDimensions.width} height={imageDimensions.height} />
-                {console.log({imageOrientation})}
-              </ImageContainer>
-            )})}
-        
+                <ImageContainer key={index} onClick={() => modalClickHandler(image.public_id, imageDimensions)} width={imageDimensions.width} height={imageDimensions.height}>
+                  <Image src={`https://res.cloudinary.com/julianb/image/upload/h_400/${image.public_id}.jpg`} alt={""} width={imageDimensions.width} height={imageDimensions.height} />
+                </ImageContainer>
+              )
+            })}
+
           </>
         )}
       </section>
-
+      {isModalOpen && (
+        <Modal modalClickHandler={modalClickHandler} modalInfo={modalInfo} />
+      )}
     </PhotoWrapper>
   )
 };
