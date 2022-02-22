3f812e54e3af0f94a637956379962fea45de476e
julimancan <julian.m.bustos@gmail.com>
2022-02-22 13:06:44 -0500

    fixes sizing on homepage and adds small scaling

--- components/HomeComponents/HomeSlideShow.js
+++ components/HomeComponents/HomeSlideShow.js
@@ -1,60 +1,62 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { wrap } from "popmotion";
-import styled from '@emotion/styled';
-import Image from "next/image";
+import styled from "@emotion/styled";
+// import Image from "next/image";
 
-const ImageComp = styled(Image)`
+const ImageComp = styled.div`
+  overflow: hidden;
   position: absolute;
   width: 100%;
   height: 100vh;
-  margin-left: auto;
-  margin-right: auto;
-  object-fit: cover;
-  top: 0;
-  left: 0;
-  filter: grayscale(100%);
-  /* opacity: 1; */
-  background: black;
-  background-size: 100vw 100% cover;
-  background-position: center;
-  animation: changeOpacity 4s infinite;
-  transition: opacity .2s ease-in-out;
-  /* @keyframes changeOpacity {
-    from {
-       opacity: .5; 
+  img {
+    width: 100%;
+    height: 100vh;
+    margin-left: auto;
+    margin-right: auto;
+    object-fit: cover;
+    top: 0;
+    left: 0;
+    filter: grayscale(100%);
+    /* opacity: 1; */
+    background: black;
+    background-size: 100vw 100% cover;
+    background-position: center;
+    animation: changeOpacity 5s infinite;
+    /* transition: opacity 0.2s ease-in-out; */
+    @keyframes changeOpacity {
+      from {
+        transform: scale(1);
+      }
+      to {
+        transform: scale(1.1);
+      }
     }
-    to {
-      opacity: 1;
-    }
-  } */
-`
+  }
+`;
 
-
-
-const HomeSlideShow = ({photos}) => {
+const HomeSlideShow = ({ photos }) => {
   const [page, setPage] = useState(0);
   const imageIndex = wrap(0, photos.length, page);
 
   setTimeout(() => {
     const newPage = page + 1;
-    setPage(newPage)
+    setPage(newPage);
   }, 4000);
-  // console.log(images)
+  console.log({ photos });
   return (
-    <>
-      {photos.length > 0 && (
-        <ImageComp
+    <ImageComp>
+      {photos && (
+        <img
           key={page}
-          src={photos.length > 0 && photos[imageIndex].url}
+          src={photos && photos[imageIndex].url}
           alt="Black Jade Image Background"
           // width={photos[imageIndex].width}
           // height={photos[imageIndex].height}
-          layout='fill'
+          // layout='fill'
         />
-      )
-      }
-    </>
-  )
-}
+      )}
+    </ImageComp>
+  );
+};
 
-export default HomeSlideShow
+export default HomeSlideShow;
