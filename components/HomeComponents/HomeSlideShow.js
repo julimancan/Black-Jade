cdbad64aa8a61394c1f12b8c69fe5ca37696fb88
julimancan <julian.m.bustos@gmail.com>
2022-02-23 10:22:10 -0500

    fixed the homepage image animation

--- components/HomeComponents/HomeSlideShow.js
+++ components/HomeComponents/HomeSlideShow.js
@@ -1,65 +1,69 @@
 import React, { useEffect, useState } from "react";
 import { wrap } from "popmotion";
 import styled from "@emotion/styled";
-// import Image from "next/image";
+import { useInterval } from 'usehooks-ts'
 
 const ImageComp = styled.div`
   overflow: hidden;
   position: absolute;
   width: 100%;
   height: 100vh;
-  img {
-    width: 100%;
-    height: 100vh;
-    margin-left: auto;
-    margin-right: auto;
-    object-fit: cover;
-    top: 0;
-    left: 0;
-    filter: grayscale(80%);
-    /* opacity: 1; */
-    background: black;
-    background-size: 100vw 100% cover;
-    background-position: center;
-    animation: changeOpacity 2s infinite;
-    animation: changeScale 5s infinite;
-    transition: opacity 0.2s ease-in-out;
-    @keyframes changeScale {
-      0% {
-        transform: scale(1);
-        opacity: 0
-      }
-      25% {
-        opacity: 1;
-      }
-      100% {
-        transform: scale(1.1);
-      }
+`;
+
+const StyledImage = styled.img`
+  width: 100vw;
+  height: 100vh;
+  margin-left: auto;
+  margin-right: auto;
+  object-fit: cover;
+  top: 0;
+  left: 0;
+  filter: grayscale(80%);
+  position: absolute;
+  background: black;
+  background-size: 100vw 100% cover;
+  background-position: center;
+  opacity: ${({ selected }) => (selected ? 1 : 0)};
+  transition: opacity 5s;
+  animation: changeScale 6s infinite alternate-reverse;
+  @keyframes changeScale {
+    0% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(1.1);
     }
   }
 `;
 
 const HomeSlideShow = ({ photos }) => {
-  const [page, setPage] = useState(0);
-  const imageIndex = wrap(0, photos.length, page);
+  const [selected, setSelected] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false)
+  const nextImage = () => {
+    setSelected((prev) => (prev === photos.length - 1 ? 0 : prev + 1));
+  };
 
-  setTimeout(() => {
-    const newPage = page + 1;
-    setPage(newPage);
-  }, 4000);
-  console.log({ photos });
+  useEffect(() => {
+    setIsPlaying(true)
+  }, []);
+
+  useInterval(
+    () => {
+      nextImage()
+    },
+    isPlaying ? 6000 : null
+  )
   return (
-    <ImageComp>
-      {photos && (
-        <img
-          key={page}
-          src={photos && photos[imageIndex].url}
-          alt="Black Jade Image Background"
-          // width={photos[imageIndex].width}
-          // height={photos[imageIndex].height}
-          // layout='fill'
+    <ImageComp arrLength={photos.length}>
+      {photos.map((photo, index) => (
+        <StyledImage
+          key={index}
+          src={photo.url}
+          alt={`Black Jade Image Background ${index + 1}`}
+          selected={selected === index}
+          onClick={nextImage}
         />
-      )}
+      ))}
     </ImageComp>
   );
 };
